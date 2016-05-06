379faae9e100191e094edd60fef2ea5b8a2f6d12
Anuradha Chaturvedi <chaturanu@gmail.com>
2016-05-05 22:01:30 -0600

    section 6 animations

---
+++ section6_animations/section6_animations/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  section6_animations
+//
+//  Created by Anuradha Chaturvedi on 5/5/16.
+//  Copyright © 2016 anuchaturvedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var frameNum = 1
+    
+    @IBOutlet var singingKanye: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func makeKanyeSing(sender: AnyObject)
+    {
+        
+        frameNum += 1
+        
+        singingKanye.image = UIImage(named: "frame" + String(frameNum) + ".png")
+    }
+}
+
